ffa19991e3fa922889bb745aded40302b5bc69c9
danadi87 <dragulescumirela@gmail.com>
2023-03-15 11:50:49 +0100

    added a Footer

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <h6>
+      <a
+        href="https://github.com/danadi87/React-Weather-Project"
+        target="_blank"
+      >
+        Open-source code
+      </a>
+      {"by Mirela Dragulescu"}
+    </h6>
+  );
+}
